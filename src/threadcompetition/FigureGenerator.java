08c06c2a537fc54ca7e3d13f8c2b54024e3ea437
Elías Orozco <elias.orozco95@gmail.com>
2018-08-25 16:02:47 -0600

    generation of figures

---
+++ src/threadcompetition/FigureGenerator.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threadcompetition;
+
+/**
+ *
+ * @author Elias
+ */
+public class FigureGenerator {
+    
+    
+    public void paramGenerator(int figureQty, int speedSelection){
+        int figureFlag = 0;
+        while(figureFlag<figureQty){
+            //Select road();
+            //ThreadFigure tf = new ThreadFigure(Road.getx, Road.gety, speedSelection);
+            //Road.add(tf);
+            
+        }
+    }
+    
+    /*
+    public Road selectRoad(){
+        
+    }
+    */
+    
+    
+}
